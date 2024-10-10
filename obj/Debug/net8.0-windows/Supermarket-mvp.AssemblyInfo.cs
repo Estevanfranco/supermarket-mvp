fbcc923e0193713da0312548df0790f0d6cde1dc
estevan franco abadia <drago2062@gmail.com>
2024-10-10 16:41:33 -0500

    Pay Mode Presenter

--- obj/Debug/net8.0-windows/Supermarket-mvp.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/Supermarket-mvp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Supermarket-mvp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3d5b54ecfde9e06f94c3319f0cecb5dcc4a6395")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90e32d761c8711601c757517c1f9b126a3c86a98")]
 [assembly: System.Reflection.AssemblyProductAttribute("Supermarket-mvp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Supermarket-mvp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
